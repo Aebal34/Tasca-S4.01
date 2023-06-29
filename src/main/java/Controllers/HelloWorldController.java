a1d29bfb2b8eac424117e5593f3958ae3969119b
Joren <97386268+Aebal34@users.noreply.github.com>
2023-06-29 20:26:49 +0200

    Project start

---
+++ src/main/java/Controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package Controllers;
+
+public class HelloWorldController {
+
+    public String greetings(String name){
+        return "Hello "+name+". You are executing a Maven project.";
+    }
+
+    public String greetings2(String name){
+        return "Hello "+name+". You are executing a Maven project.";
+    }
+
+}
